b488d9ffd81357c0f3850ace2a58ad4548d099ed
umaqgeek <umaqgeek@gmail.com>
2020-12-27 19:08:32 +0800

    chore: update vendor file

--- src/Vendor.js
+++ src/Vendor.js
@@ -1,29 +1,26 @@
-var parentPath = '../../../../';
-var projectPath = parentPath+'node_modules/@umaqgeek/mywebmin/src/assets/';
-
 //Bootstrap 4.0.0
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css';
 
 //Jquery
-import (parentPath+'node_modules/jquery/dist/jquery.js');
+import '../../../node_modules/jquery/dist/jquery.js';
 
 //Reactstrap Js
-import (projectPath+'js/reactstrap.min.js');
+import './js/reactstrap.min.js';
 
 //React Date Picker
 import '!style-loader!css-loader!react-datepicker/dist/react-datepicker.css';
 
 //Font Awesome 
-import ('!style-loader!css-loader!'+parentPath+'node_modules/font-awesome/css/font-awesome.min.css');
+import '!style-loader!css-loader!../../../node_modules/font-awesome/css/font-awesome.min.css';
 
 //Accordion CSS
-import '!style-loader!css-loader!react-accessible-accordion/dist/fancy-example.css';
+import '!style-loader!css-loader!../../../node_modules/react-accessible-accordion/dist/fancy-example.css';
 
 //React Big Calender
-import  ('!style-loader!css-loader!'+parentPath+'node_modules/react-big-calendar/lib/css/react-big-calendar.css');
+import '!style-loader!css-loader!../../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
 
 //Themify Icon Css
-import (projectPath+'assets/themify-icons/themify-icons.css');
+import './themify-icons/themify-icons.css';
 
 //Themify Icon Css
-import (projectPath+'assets/js/custom.js');
+import './js/custom.js';
